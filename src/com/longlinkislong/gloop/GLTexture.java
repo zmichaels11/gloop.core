5862b14d0ffffbc57ef428cd1ba7dfbc409c1b34
Robert <Robert@Runouw-PC3>
2015-07-03 22:34:49 -0700

    Replaced GL_MAX_TEXTURE_UNITS with GL_MAX_TEXTURE_IMAGE_UNITS.

--- src/com/longlinkislong/gloop/GLTexture.java
+++ src/com/longlinkislong/gloop/GLTexture.java
@@ -12,6 +12,7 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL21;
 import org.lwjgl.opengl.GL30;
 import org.lwjgl.opengl.GL31;
@@ -1126,7 +1127,7 @@ public class GLTexture extends GLObject {
                 return this.maxUnits;
             }
 
-            return this.maxUnits = GL11.glGetInteger(GL13.GL_MAX_TEXTURE_UNITS);
+            return this.maxUnits = GL11.glGetInteger(GL20.GL_MAX_TEXTURE_IMAGE_UNITS);
         }
     }
     
