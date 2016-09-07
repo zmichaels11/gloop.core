a46c48f4105964be3393db98d5858df52fb0da6d
zmichaels <zmichaels11@gmail.com>
2016-09-07 12:26:08 -0700

    - updated licenses

--- src/main/java/com/longlinkislong/gloop/GLProgram.java
+++ src/main/java/com/longlinkislong/gloop/GLProgram.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2015, longlinkislong.com
+ * Copyright (c) 2014-2016, longlinkislong.com
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
